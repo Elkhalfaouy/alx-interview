3b14626ea548f4f89d18bebe393c7b235f236aee
Elkhalfaouy <elkhalfaouy.amine@outlook.com>
2024-07-21 13:42:09 -0700

    ok

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
-"""
+
+"""A module that rotates a matrix by 90 degrees"""
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
-    """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    """returns a 90 degrees rotated matrix"""
+
+    copy_matrix = matrix.copy()
+    matrix.clear()
+
+    copy_matrix.reverse()
+
+    for idx in range(len(copy_matrix)):
+        temp_row = [element[idx] for element in copy_matrix]
+        matrix.append(temp_row)
